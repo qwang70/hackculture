770f78a38cd57f008ea888bd9bbbf7344da6a6ff
qwang70 <qiwen.wang@163.com>
2018-04-13 23:18:14 -0500

    update database

---
+++ query.sql
@@ -0,0 +1,6 @@
+.separator "," 
+.import csv/13-14-Graybook/employee.csv year2013
+.import csv/14-15-Graybook/employee.csv year2014
+.import csv/15-16-Graybook/employee.csv year2015
+.import csv/16-17-Graybook/employee.csv year2016
+.import csv/17-18-Graybook/employee.csv year2017
--- scrape.py
+++ scrape.py
@@ -11,8 +11,48 @@ html_path = html_folder + year
 csv_path = csv_folder + year
 html_files = [f for f in listdir(html_path) if isfile(join(html_path, f)) \
              and "html" in f and "TOC" not in f]
-
-labels = ["College", "Campus", "Department", "Employee Name", "Job Title", "Tenure", "Empl Class",  "Salary"]
+titlesToInclude = [
+   "ASSOC PROF UNIV LIBRARY",
+   "ASST PROF (CT)",
+   "ASST PROF UNIV LIBRARY",
+   "PROF (CT)",
+   "JL DOOB RES ASST PROF",
+   "VST LECTURER",
+   "ASSOC PROF (CT)",
+   "RES PROF",
+   "VST CLIN ASST PROF",
+   "VST ASST PROF",
+   "ASST DEAN",
+   "SR LECTURER",
+   "RES ASSOC PROF",
+   "RES SCI",
+   "ASST PROF (RT)",
+   "CLIN PROF",
+   "DEAN",
+   "VST RES ASST PROF",
+   "SR RES SCI",
+   "ASSOC PROF (RT)",
+   "PROF (RT)",
+   "CLIN ASSOC PROF",
+   "ASSOC DIR",
+   "RES ASST PROF",
+   "CLIN ASST PROF",
+   "POSTDOC RES ASSOC",
+   "LECTURER",
+   "ASST PROF",
+   "ASSOC PROF",
+   "PROF"]
+tenure_dict = {
+    "": "No Info",
+    "A": "Indefinite Tenure",
+    "M": "Multi-Year Contract Agreement",
+    "N": "Initial/Partial Term",
+    "P": "Probationary Term",
+    "Q": "Specified Term Appointment",
+    "T": "Terminal Contract",
+    "W": "Special"
+}
+labels = ["College", "Campus", "Department", "EmployeeName", "JobTitle", "Tenure",  "Salary"]
 Urbana = ["KL", "KY", "LD", "NQ", "LT", "LN", "NA", "NT", "KM", "KT", "NU",\
          "KW", "KN", "MY", "KP", "NN", "KR", "KS", "LQ", "KU", "KV", "LB", \
          "NS", "NB", "LM", "NH", "LF", "LP", "LG", "LL", "NC", "LR", "NJ", \
@@ -60,6 +100,7 @@ for html_file in html_files:
         # get college name
         college_col = soup.find("table").tbody.tr
         college = college_col.get_text(strip=True)
+        college = college.split(" - ")[1]
         # get the next row
         curr_row = college_col.find_next_sibling("tr")
         while(curr_row):
@@ -67,6 +108,7 @@ for html_file in html_files:
                 print(len(curr_row.get_text(strip=True)))
             elif curr_row.find("td") is None:
                 curr_dep = curr_row.find("th").get_text(strip=True)
+                curr_dep = curr_dep.split(" - ")[1]
                 print(curr_dep)
             elif not curr_row.find("th") or\
                 "Employee Total for All Jobs..." \
@@ -78,14 +120,14 @@ for html_file in html_files:
                 employee_info = curr_row.find_all("td")
                 job_title = employee_info[0].get_text(strip=True)
                 tenure = employee_info[1].get_text(strip=True)
-                empl_class = employee_info[2].get_text(strip=True)
+                tenure = tenure_dict[tenure]
                 fte = float(employee_info[4].get_text(strip=True))
                 salary = get_salary(employee_info[5].get_text(strip=True))
 
                 # if Proposed FTE is 1, which means full time
                 # TODO: can be determined later
-                if fte == 1:
-                    new_employee =  (college, curr_campus, curr_dep, curr_employee, job_title, tenure, empl_class, salary) 
+                if fte == 1 and job_title in titlesToInclude:
+                    new_employee =  (college, curr_campus, curr_dep, curr_employee, job_title, tenure, salary) 
                     employees.append(new_employee)
             curr_row = curr_row.find_next_sibling("tr")
         # list to dataframe
